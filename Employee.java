1e500819bc73cbe67206ed7c732a5fde1b1edd78
kanishkkasri g <01fe22bca059@kletech.ac.in>
2024-03-31 20:02:16 +0530

    Displays employee bonus

---
+++ Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+    private String name;
+    private int employeeId;
+    protected double salary;
+
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        // Creating an employee
+        Employee employee = new Employee("Kanishkkasri g", 12345, 50000);
+        System.out.println("Employee Details:");
+        employee.displayDetails();
+        
+        // Creating a DevOps Engineer
+        DevOpsEngineer devOpsEngineer = new DevOpsEngineer("Kruthikha g", 54321, 60000, 10000);
+        System.out.println("\nDevOps Engineer Details:");
+        devOpsEngineer.displayDetails();
+    }
+}
